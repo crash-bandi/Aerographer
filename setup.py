086c35dd068068085c6c0b1377b1a0b5b6ce0c04
Jason Lines <jason.lines@saks.com>
2023-01-04 09:06:57 -0600

    reset version to v0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='aerographer',
-    version='1.2.4',
+    version='0.0.1',
     python_requires='>=3.10',
     install_requires=['boto3 >=1.21.0'],
     package_data={'aerographer': ['*.json',  '*.pyi', '**/*.pyi']},
